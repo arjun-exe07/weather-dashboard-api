c0e5b16ea13a8c3d786560cb664897f5d4d5b6a6
arjun-exe07 <arjunkadam202007@gmail.com>
2026-08-08 10:10:21 +0530

    Added comments to better understand the schema

--- schemas.py
+++ schemas.py
@@ -1,24 +1,29 @@
 from pydantic import BaseModel, Field
 
+#Request Schema
 class UserCreate(BaseModel):
     username: str = Field(min_length = 3, max_length=50)
     password: str = Field(min_length=6, max_length=100)
 
+#Response Schema
 class UserResponse(BaseModel):
     id: int
     username: str
     class Config:
         from_attributes = True
 
+#Response Schema
 class Token(BaseModel):
     access_token: str
     token_type: str
 
+#Request Schema
 class FavoriteCityCreate(BaseModel):
     city_name: str
     latitude: str
     longitude: str
 
+#Response Schema
 class FavoriteCityResponse(BaseModel):
     id: int
     city_name: str
@@ -27,6 +32,7 @@ class FavoriteCityResponse(BaseModel):
     class Config:
         from_attributes = True
 
+#Response Schema
 class WeatherResponse(BaseModel):
     city: str
     temperature: float
